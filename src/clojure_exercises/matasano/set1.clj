d0aba623621bf608f3f49457b7847bb85d7ebe8c
Cassiano Alves <cassiano.alves@gmail.com>
2014-09-22 01:28:15 -0300

    Starting new approach

--- src/clojure_exercises/matasano/set1.clj
+++ src/clojure_exercises/matasano/set1.clj
@@ -1,40 +1,18 @@
 (ns clojure-exercises.matasano.set1
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
 ;; Convert hex to base64
 
 (def hexMap
-    (apply assoc {} (interleave
-     (vec (.getBytes "0123456789abcdef"))
-     (vec (range 0 16)))))
+    (apply array-map (interleave "0123456789abcdef" (range 0 16))))
 
 (def base64Map
-    (apply assoc {} (interleave
-     (vec (.getBytes "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"))
-     (vec (range 0 64)))))
+    (apply array-map (interleave "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" (range 0 64))))
 
-(defn hexByteToValue
-  [in]
-  (+
-   (* 16 (hexMap (first in)))
-   (hexMap (second in))))
-  ;(+ (* 16 (hexMap (first in))) (hexMap (second in))))
-
-(defn hex2bytes [hex]
-           (partition 2
-                       (.getBytes (.toLowerCase hex)) ))
-
+(defn hex2nibbleseq [hex-str]
+  (map hexMap (str/lower-case hex-str)))
 
 (defn hex2base64 [in]
-  (hex2bytes in))
+  "")
 
-
-(first '(51 48))
-(hexByteToValue '(49 49))
-(partition 2 "3031320A")
-
-(str (vec (\2 \a)))
-(hex2bytes "3031320A")
-
-
-(seq-map (interleave '(\1 \2) '(1 2)))
--- test/clojure_exercises/matasano/set1_test.clj
+++ test/clojure_exercises/matasano/set1_test.clj
@@ -2,32 +2,13 @@
   (:require [clojure.test :refer :all]
             [clojure-exercises.matasano.set1 :refer :all]))
 
-(deftest hexByteToValue-test
-  (testing "hexByteToValue")
-  (is (= (hexByteToValue (seq (.getBytes "2A"))) 42)))
+(deftest hex2nibbleseq-test
+  (testing "hex2nibbleseq")
+  (is (= (hex2nibbleseq "12AB") '(1 2 10 11))))
 
-(deftest hex2bytes-test
-  (testing "hex2bytes")
-  (is (=
-       (hex2bytes "303132")
-         '(48 49 50))))
 
 (deftest hex2base64-test
   (testing "Set1 - Chalenge 1 - Convert hex to base64"
-
     (is (= (hex2base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))))
 
-
-
-(hexByteToValue '(48 49))
-
-'(50 65)
-
-(seq (.getBytes "2A"))
-
-(def inValue (map int (seq "2A")))
-
-(hexByteToValue inValue)
-
-(def a (int \2))
