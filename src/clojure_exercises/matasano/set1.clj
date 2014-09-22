dcb78b7c75ef1278ebe2611c4dff763e1df990f4
Cassiano Alves <cassiano.alves@b2wdigital.com>
2014-09-22 14:53:11 -0300

    Regrouping bits for base64

--- src/clojure_exercises/matasano/set1.clj
+++ src/clojure_exercises/matasano/set1.clj
@@ -10,8 +10,19 @@
 (def base64Map
     (apply array-map (interleave "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" (range 0 64))))
 
+(defn divideNibble [nib]
+  (seq [(bit-shift-right nib 2) (bit-and nib 2r0011)]))
+
 (defn hex2nibbleseq [hex-str]
   (map hexMap (str/lower-case hex-str)))
+
+(defn hex2HalfNibble [hex]
+  (apply concat
+         (map divideNibble
+              (hex2nibbleseq hex))))
+
+(defn halfNibble2SixBits [hn]
+  (partition-all 3 hn))
 
 (defn hex2base64 [in]
   "")
